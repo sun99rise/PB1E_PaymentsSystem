94d2bb244eb52bf44debdbaf3f12175c2a76612c
sun99rise <suraj99r@gmail.com>
2022-07-25 15:02:11 +0530

    packages and intial classes

---
+++ src/main/java/com/barclays/paymentsSystem/controller/LoginController.java
@@ -0,0 +1,5 @@
+package com.barclays.paymentssystem.controller;
+
+public class LoginController {
+
+}
---
+++ src/main/java/com/barclays/paymentsSystem/repository/LoginRepository.java
@@ -0,0 +1,5 @@
+package com.barclays.paymentssystem.repository;
+
+public interface LoginRepository {
+
+}
