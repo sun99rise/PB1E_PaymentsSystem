fd9fa4a3461d80a49fff591583354f4458bffc4a
sun99rise <suraj99r@gmail.com>
2022-07-25 14:57:15 +0530

    project Structure

--- src/main/java/com/barclays/paymentsSystem/Pb1EPaymentsSystemApplication.java
+++ src/main/java/com/barclays/paymentsSystem/Pb1EPaymentsSystemApplication.java
@@ -1,4 +1,4 @@
-package com.barclays.paymentsSystem;
+package com.barclays.paymentssystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/barclays/paymentsSystem/entity/BillerMasterList.java
@@ -0,0 +1,5 @@
+package com.barclays.paymentssystem.entity;
+
+public class BillerMasterList {
+
+}
