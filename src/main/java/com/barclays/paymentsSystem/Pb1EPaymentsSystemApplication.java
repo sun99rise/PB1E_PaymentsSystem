2082b1e4fff10f64e520d0beee29c06283cf635b
sun99rise <suraj99r@gmail.com>
2022-07-25 11:22:50 +0530

    pb1e payments system project intial commit

---
+++ src/main/java/com/barclays/paymentsSystem/Pb1EPaymentsSystemApplication.java
@@ -0,0 +1,13 @@
+package com.barclays.paymentsSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pb1EPaymentsSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pb1EPaymentsSystemApplication.class, args);
+	}
+
+}
