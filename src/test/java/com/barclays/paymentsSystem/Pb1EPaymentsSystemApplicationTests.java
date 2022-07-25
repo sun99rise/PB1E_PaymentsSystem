ee4d0eaba08ed1d5ac2f886fa961bca372a71872
sun99rise <suraj99r@gmail.com>
2022-07-25 10:40:13 +0530

    PB1E_payments system initial commit

---
+++ src/test/java/com/barclays/paymentsSystem/Pb1EPaymentsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barclays.paymentsSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pb1EPaymentsSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
