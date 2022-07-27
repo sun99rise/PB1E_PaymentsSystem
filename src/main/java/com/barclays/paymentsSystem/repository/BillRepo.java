9ed7c1a0f3116a78f6c6e5ecc27aad541e69aedf
Deepanshu1810 <94223449+Deepanshu1810@users.noreply.github.com>
2022-07-27 11:10:25 +0530

    re-committing

--- src/main/java/com/barclays/paymentsSystem/repository/BillRepo.java
+++ src/main/java/com/barclays/paymentsSystem/repository/BillRepo.java
@@ -8,18 +8,31 @@ import org.springframework.stereotype.Repository;
 
 import com.barclays.paymentssystem.entity.*;
 
+/**
+Connecting to route database using Jpa repository 
+*/
+
 @Repository
 public interface BillRepo extends JpaRepository<Bill, Integer> {
+	
+//findByAccountNumberAndStatus : FETCHING BILL DETAILS : ACCOUNT NUMBER AND STATUS
 
 	public List<Bill> findByAccountNumberAndStatus(int accountNumber, String status);
+
+//findByPrimarKeyBillerCodeAndStatusAndAccountNumber:FETCHING BILL DETAILS : ACCOUNT NUMBER,BILLER CODE AND STATUS
 
 	public List<Bill> findByPrimarKeyBillerCodeAndStatusAndAccountNumber(String billerCode, String status,
 			int accountNumber);
 
+//findByPrimarKeyBillerCodeAndPrimarKeyConsumerNumberAndAccountNumberAndStatus : FETCHING BILL DETAILS : ACCOUNT NUMBER,BILLER CODE,CONSUMER NUMBER AND STATUS
+
 	public Optional<Bill> findByPrimarKeyBillerCodeAndPrimarKeyConsumerNumberAndAccountNumberAndStatus(
 			String billerCode, String consumerNumber, int accountNumber, String status);
 
+//findByStatus :FETCHING BILL DETAILS: STATUS
+
 	public List<Bill> findByStatus(String status);
+
 	
 	public Optional<Bill> findByPrimarKey(PrimaryKeyForBill billKey);
 
