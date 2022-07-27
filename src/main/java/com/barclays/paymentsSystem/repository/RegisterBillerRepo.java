42433f27e79d19b474cf56de1864b31aa69b51a8
paksham98 <52401492+paksham98@users.noreply.github.com>
2022-07-27 10:34:08 +0530

    Update RegisterBillerRepo.java

--- src/main/java/com/barclays/paymentsSystem/repository/RegisterBillerRepo.java
+++ src/main/java/com/barclays/paymentsSystem/repository/RegisterBillerRepo.java
@@ -8,7 +8,9 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.barclays.paymentssystem.entity.RegisteredBillers;
-// Connecting to route database using jpa repository class
+/**
+Connecting to route database using jpa repository class
+*/
 @Repository
 public interface RegisterBillerRepo extends JpaRepository<RegisteredBillers, String> {
 
@@ -16,7 +18,9 @@ public interface RegisterBillerRepo extends JpaRepository<RegisteredBillers, Str
 	// @Query("delete from registeredBiller where bill_sequence_id = ?1")
 	int deleteBySequenceid(String id);
 	
-// Retrieving registerbiller as true and paylimit greater than bill amount for given biller code and consumer number
+/** 
+Retrieving registerbiller as true and paylimit greater than bill amount for given biller code and consumer number
+*/
 	
 	public RegisteredBillers findByAccountNumberAndPrimarKeyBillerCodeAndPrimarKeyConsumerNumberAndAutoPayTrueAndAutoPayLimitGreaterThan(int accountNumber,
 			String billerCode, String consumerNumber,double billAmount);
